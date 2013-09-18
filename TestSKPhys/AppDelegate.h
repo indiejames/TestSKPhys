69eac14bafb9a0c21d7c87b7117895e4307c0139
James Norton <jamesnorton@gmail.com>
2013-09-17 20:14:55 -0400

    Initial Commit

---
+++ TestSKPhys/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSKPhys
+//
+//  Created by James Norton on 9/17/13.
+//  Copyright (c) 2013 James Norton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
