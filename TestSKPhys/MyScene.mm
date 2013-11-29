32c7dd973e64bd112c92bf5147b767f71957e238
James Norton <jamesnorton@gmail.com>
2013-11-28 23:14:12 -0500

    Modified to use bigger boxes for repeatability test

--- TestSKPhys/MyScene.mm
+++ TestSKPhys/MyScene.mm
@@ -9,43 +9,31 @@
 #import "MyScene.h"
 #import <Box2D.h>
 
-//#define BOX2D
-#define NUM_BOXES (200)
+#define BOX2D
+#define NUM_BOXES (150)
 #define PIXELS_PER_METER (150)
-#define BOX_WIDTH (20)
-#define BOX_HEIGHT (BOX_WIDTH / 2)
+#define SMALL_BOX_WIDTH (16)
+#define BIG_BOX_WIDTH (32)
+#define SMALL_BOX_HEIGHT (SMALL_BOX_WIDTH / 2)
+#define BIG_BOX_HEIGHT (BIG_BOX_WIDTH / 2)
 #define ANGULAR_VELOCITY (25)
-#define ANGULAR_DAMPING (0.1)
+#define ANGULAR_DAMPING (0)
+#define LINEAR_DAMPING (0)
 #define DENSITY (1.0)
-#define FRICTION (0.2)
-#define RESTITUTION (0.2)
-#define GRAVITY (-4.5)
-#define MOV_AVG_COUNT (10)
-#define FIRST_SAMPLE_POINT (0.5)
-#define SECOND_SAMPLE_POINT (10.0)
+#define FRICTION (0.1)
+#define RESTITUTION (0.1)
+#define GRAVITY (-1.5)
+
+float boxWidth = BIG_BOX_WIDTH;
+float boxHeight = BIG_BOX_HEIGHT;
 
 @implementation MyScene {
     b2World *world;
     double accumulator;
     double timeStep;
     double prevTime;
-    int frameCount;
-    double frameTimeTotal;
-    double frameRate[MOV_AVG_COUNT];
     float g;
     BOOL frameRateDisplayed;
-}
-
--(void)addBoxes {
-    float xStart = BOX_WIDTH;
-    float yStart = self.size.height-BOX_HEIGHT - 30;
-    
-    for(int i = 0;i<NUM_BOXES;i++) {
-        float x = (i % 15) * BOX_WIDTH + xStart;
-        float y = yStart - (i / 15) * (BOX_HEIGHT + 10);
-        [self addRockAtX:x Y:y];
-        
-    }
 }
 
 -(id)initWithSize:(CGSize)size {    
@@ -54,8 +42,6 @@
         timeStep = 1.0 / 60.0;
         accumulator = 0;
         prevTime = -1;
-        frameCount = 0;
-        frameTimeTotal = 0;
         g = GRAVITY;
         frameRateDisplayed = NO;
         
@@ -101,23 +87,37 @@
     return self;
 }
 
+-(void)addBoxes {
+    float xStart = boxWidth - 3;
+    float yStart = self.size.height-2*boxHeight;
+    
+    int rowSize = self.size.width / (boxWidth + 5);
+    
+    for(int i = 0;i<NUM_BOXES;i++) {
+        float x = (i % rowSize) * (boxWidth + 5) + xStart;
+        float y = yStart - (i / rowSize) * (boxHeight * 1.5);
+        [self addBoxAtX:x Y:y];
+        
+    }
+}
 
-- (void)addRockAtX:(float )x Y:(float)y {
+
+- (void)addBoxAtX:(float )x Y:(float)y {
     
     
     
     
-    SKSpriteNode *rock = [[SKSpriteNode alloc] initWithColor:[SKColor whiteColor] size:CGSizeMake(BOX_WIDTH,BOX_HEIGHT)];
+    SKSpriteNode *Box = [[SKSpriteNode alloc] initWithColor:[SKColor whiteColor] size:CGSizeMake(boxWidth,boxHeight)];
     
-    rock.position = CGPointMake(x, y);
+    Box.position = CGPointMake(x, y);
     
-    rock.name = @"rock";
+    Box.name = @"Box";
     
 #ifdef BOX2D
     // Box2D
     
     b2BodyDef bodyDef;
-    bodyDef.position.Set(rock.position.x / PIXELS_PER_METER, rock.position.y / PIXELS_PER_METER);
+    bodyDef.position.Set(Box.position.x / PIXELS_PER_METER, Box.position.y / PIXELS_PER_METER);
     //bodyDef.angle = DEG_TO_RAD(obj.rotation);
     
     b2BodyType type = b2_dynamicBody;
@@ -138,7 +138,7 @@
         
         
     
-    polyShape.SetAsBox(rock.size.width / 2.0 / PIXELS_PER_METER, rock.size.height / 2.0 / PIXELS_PER_METER);
+    polyShape.SetAsBox(Box.size.width / 2.0 / PIXELS_PER_METER, Box.size.height / 2.0 / PIXELS_PER_METER);
     fixtureDef.shape = &polyShape;
     
     fixtureDef.density = DENSITY;
@@ -148,23 +148,25 @@
     
     body->CreateFixture(&fixtureDef);
     
-    body->SetUserData((__bridge void*)rock);
+    body->SetUserData((__bridge void*)Box);
     
     //body->SetAngularVelocity(6.28);
     body->SetAngularVelocity(ANGULAR_VELOCITY);
     body->SetAngularDamping(ANGULAR_DAMPING);
+    body->SetLinearDamping(LINEAR_DAMPING);
     
 #else
-    rock.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:rock.size];
-    rock.physicsBody.angularVelocity = ANGULAR_VELOCITY;
-    rock.physicsBody.angularDamping = ANGULAR_DAMPING;
-    rock.physicsBody.restitution = RESTITUTION;
-    rock.physicsBody.density = DENSITY;
-    rock.physicsBody.friction = FRICTION;
+    Box.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:Box.size];
+    Box.physicsBody.angularVelocity = ANGULAR_VELOCITY;
+    Box.physicsBody.angularDamping = ANGULAR_DAMPING;
+    Box.physicsBody.restitution = RESTITUTION;
+    Box.physicsBody.density = DENSITY;
+    Box.physicsBody.friction = FRICTION;
+    Box.physicsBody.linearDamping = LINEAR_DAMPING;
     
 #endif
     
-    [self addChild:rock];
+    [self addChild:Box];
     
 }
 
@@ -173,9 +175,6 @@
 void updatePhysics(double deltaT, double &accumulator, double timeStep, b2World *world) {
     int velocityIterations = 8;
     int positionIterations = 3;
-    //int velocityIterations = 10;
-    //int positionIterations = 10;
-    
     
     if (deltaT > 0.25) {
         deltaT = 0.25;// note: max frame time to avoid spiral of death
@@ -199,8 +198,6 @@ void updatePhysics(double deltaT, double &accumulator, double timeStep, b2World 
                 
                 sprite.zRotation = angle;
                 
-                
-                //GemLog(@"(x,y,theta) = (%4.2f, %4.2f, %4.2f)\n", position.x, position.y, angle);
             }
            
             
@@ -223,8 +220,6 @@ void updatePhysics(double deltaT, double &accumulator, double timeStep, b2World 
         CGPoint pos = CGPointMake((position.x * PIXELS_PER_METER +(1.0 - alpha) * sprite.position.x/PIXELS_PER_METER),
                                   (position.y * PIXELS_PER_METER + (1.0 - alpha)*sprite.position.y/PIXELS_PER_METER));
         sprite.position = pos;
-        
-        //NSLog(@"x = %f   y = %f", pos.x, pos.y);
         
         float32 angle = b->GetAngle();
         sprite.zRotation = alpha * angle + (1.0-alpha)*sprite.zRotation;
@@ -253,33 +248,10 @@ void updatePhysics(double deltaT, double &accumulator, double timeStep, b2World 
         #ifdef BOX2D
         updatePhysics(deltaT, accumulator, timeStep, world);
         #endif
-        /*frameTimeTotal += deltaT;
         
-        frameRate[frameCount] = 1.0 / deltaT;
-        
-        frameCount++;
-        
-        if (frameCount == MOV_AVG_COUNT) {
-            double avgFrameRate = 0;
-            for(int i=0;i<frameCount;i++){
-                avgFrameRate += frameRate[i];
-            }
-            avgFrameRate = avgFrameRate / frameCount;
-            
-            if ((frameTimeTotal > FIRST_SAMPLE_POINT && frameTimeTotal < SECOND_SAMPLE_POINT && !frameRateDisplayed) || (frameTimeTotal > SECOND_SAMPLE_POINT && frameRateDisplayed)) {
-                NSLog(@"Frame rate = %f", avgFrameRate);
-                frameRateDisplayed = !frameRateDisplayed;
-            }
-            
-            
-            
-            frameCount = 0;
-        }*/
     }
     
-    
     prevTime = currentTime;
-
     
 }
 
